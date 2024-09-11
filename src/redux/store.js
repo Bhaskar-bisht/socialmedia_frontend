78fb4add65f3d4b70b8ca8eafcaaccd16716db03
Bhaskar-bisht <139526056+Bhaskar-bisht@users.noreply.github.com>
2024-09-11 21:59:58 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -33,6 +33,7 @@ const store = configureStore({
                 ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
             },
         }),
+        devTools: process.env.NODE_ENV !== 'production',
 });
 
 export default store;
